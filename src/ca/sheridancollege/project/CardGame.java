a279e077df1acd5bddaa23ad26f092e69780aa94
tanisha1313 <tanisha5@sheridancollege.ca>
2025-12-09 16:41:34 -0500

    Add files via upload

---
+++ src/ca/sheridancollege/project/CardGame.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cardgame;
+
+/**
+ *
+ * @author tanis
+ */
+public class CardGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+        private int suit;
+	public void getValue() {
+		// TODO - implement CardGame.getAttribute
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param attribute
+	 */
+	public void setValue(int suit) {
+		this.suit = suit;
+	}
+
+	private int attribute;
+}
